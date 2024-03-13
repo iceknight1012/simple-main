3da4b972c99af0c72a06fd99548e78c3e35d0569
iceknight1012 <139167296+iceknight1012@users.noreply.github.com>
2024-03-13 23:42:20 +0000

    inistal commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+// addition is 1
+// suntraction is 2
+// multiplication is 3
+// division is 4
+
+int num_1;
+int num_2;
+int answer;
+
+int calculator() {
+
+    cout << "pick a number ";
+    cin >> num_1;
+    cout << "pick a second number ";
+    cin >> num_2; 
+    cout << "pick a operator ";
+    cin >> answer;
+    
+        switch(answer) {
+
+        case 1: 
+        cout << num_1 + num_2  << endl;
+        break;
+
+        case 2:
+        cout << num_1 - num_2  << endl;
+        break;
+
+        case 3:
+        cout << num_1 * num_2  << endl;
+        break;
+
+        case 4:
+        cout << num_1 / num_2 << endl;
+        break;
+
+    };
+}
 
 int main() {
 
-    cout << "hello world" << endl;
+    calculator();
 
     return 0;
 }
